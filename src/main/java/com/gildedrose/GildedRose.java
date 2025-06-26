db199d8c0c5178357a00fbac815216c9589f662b
Barhal93 <poubelle-lpw.thinly274@passmail.net>
2025-06-26 10:00:51 +0200

    Remove "static" from a method automatically created by the IDE

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -25,7 +25,7 @@ class GildedRose {
         }
     }
 
-    private static void processDefaultItem(Item item) {
+    private void processDefaultItem(Item item) {
         if (item.quality > 0) {
             item.quality = item.quality - 1;
         }
